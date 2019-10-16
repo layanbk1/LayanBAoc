1352d30a277c859fb3eaab6b0bbe8d99e986aca8
layanbk1 <layanbk1@gmail.com>
2019-10-16 11:09:27 +0300

    10

--- app/src/main/java/y2019/aoc/layanb/layanbaoc2019/AlertActivity.java
+++ app/src/main/java/y2019/aoc/layanb/layanbaoc2019/AlertActivity.java
@@ -1,7 +1,10 @@
 package y2019.aoc.layanb.layanbaoc2019;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class AlertActivity extends AppCompatActivity {
@@ -13,7 +16,23 @@ public class AlertActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
 
-        buttonPpic = (button)findViewById(R.id.buttonPpic);
-        EvantHandler();
+        buttonPpic = findViewById(R.id.buttonNext);
+        buttonPpic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder Builder = new AlertDialog.Builder(AlertActivity.this);
+                Builder.setMessage("You can click on the icon to choose a profile picture").setCancelable(false)
+                        .setNegativeButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog Alert = Builder.create();
+                Alert.show();
+
+            }
+        });
     }
+
 }
