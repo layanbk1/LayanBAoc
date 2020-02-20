b381354a5f15553ca6e492c9c7af63dba8169b25
layanbk1 <layanbk1@gmail.com>
2020-02-20 13:39:31 +0200

    123456 789 10 11 // 5.2.20 // 6.2.20 // 14.2.20 // 20.02.2020 //

--- app/src/main/java/y2019/aoc/layanb/layanbaoc2019/OverviewActivity.java
+++ app/src/main/java/y2019/aoc/layanb/layanbaoc2019/OverviewActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.Nullable;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -30,10 +31,22 @@ public class OverviewActivity extends AppCompatActivity {
     ImageButton ButtonCalendar;
     BottomNavigationView bottomNavigationView;
 
+    private  MoneyFragment moneyFrag;
+    private  TranFragment tranFrag;
+    private  SetFragment setFrag;
+    private PlanningsFragment planFrag;
+    private OverviewFragment overFrag;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_overview);
+
+        moneyFrag = new MoneyFragment();
+        tranFrag = new TranFragment();
+        setFrag = new SetFragment();
+        planFrag = new PlanningsFragment();
+        overFrag = new OverviewFragment();
 
         Calendar calendar = Calendar.getInstance();
         final int year = calendar.get(Calendar.YEAR);
@@ -56,28 +69,37 @@ public class OverviewActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.action_overview:
-                        Toast.makeText(OverviewActivity.this, "empty password or email", Toast.LENGTH_LONG).show();
-                        break;
+                        setFragment(overFrag);
+                        return true;
                     case R.id.action_transaction:
-                        Toast.makeText(OverviewActivity.this, "transact", Toast.LENGTH_LONG).show();
-                        break;
+                        setFragment(tranFrag);
+                        return true;
                     case R.id.action_plus:
-                        Toast.makeText(OverviewActivity.this, "plus", Toast.LENGTH_LONG).show();
-                        break;
+                        setFragment(moneyFrag);
+                        return true;
                     case R.id.action_plannings:
-                        Toast.makeText(OverviewActivity.this, "plannings", Toast.LENGTH_LONG).show();
-                        break;
+                        setFragment(planFrag);
+                        return true;
                     case R.id.action_walsettings:
-                        Toast.makeText(OverviewActivity.this, "set", Toast.LENGTH_LONG).show();
-                        break;
+                        setFragment(setFrag);
+                        return true;
 
+
+                    default:
+                        return false;
                 }
-                return true;
             }
         });
     }
 
-     DatePickerDialog.OnDateSetListener dateSetListener =
+    private void setFragment(Fragment fragment) {
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.main_fra,fragment);
+        fragmentTransaction.commit();
+    }
+
+    DatePickerDialog.OnDateSetListener dateSetListener =
             new DatePickerDialog.OnDateSetListener() {
                 public void onDateSet(DatePicker view, int year, int month, int day) {
                     Toast.makeText(getApplicationContext(), "selected date is " + view.getYear() +
